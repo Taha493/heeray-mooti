2bbf0da36817fafad865c69ab6802764656e1577
Taha Mahmood <69084443+Taha493@users.noreply.github.com>
2024-08-30 15:06:26 +0500

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -5,16 +5,16 @@ import './Footer.css'; // Importing the CSS file
 function Footer() {
   return (
     <footer className="footer">
-      <p>© H & Y Collections. All rights reserved 2024.</p>
-      <p>Email: hycollection6@gmail.com</p>
+      <p>© Heeray Mooti. All rights reserved 2024.</p>
+      <p>Email: mootiheeray@gmail.com</p>
       <div className="icons">
-        <a href="https://www.instagram.com/handycollection2024/" target="_blank" rel="noopener noreferrer" className="iconLink">
+        <a href="https://www.instagram.com/heeray_mooti/" target="_blank" rel="noopener noreferrer" className="iconLink">
           <FaInstagram />
         </a>
-        <a href="https://wa.me/+923025975556" target="_blank" rel="noopener noreferrer" className="iconLink">
+        <a href="https://wa.me/+923377773131" target="_blank" rel="noopener noreferrer" className="iconLink">
           <FaWhatsapp />
         </a>
-        <a href="mailto:hycollection6@gmail.com" className="iconLink">
+        <a href="mailto:mootiheeray@gmail.com" className="iconLink">
           <FaEnvelope />
         </a>
       </div>
