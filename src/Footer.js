c938b066129d437413e7b86c430e7f57a35d4dcd
Taha Mahmood <69084443+Taha493@users.noreply.github.com>
2025-05-31 02:02:57 +0500

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { FaInstagram, FaWhatsapp, FaEnvelope } from 'react-icons/fa'; // Using react-icons for icons
-import './Footer.css'; // Importing the CSS file
+import React from "react";
+import { FaInstagram, FaWhatsapp, FaEnvelope } from "react-icons/fa"; // Using react-icons for icons
+import "./Footer.css"; // Importing the CSS file
 
 function Footer() {
   return (
@@ -8,10 +8,20 @@ function Footer() {
       <p>© Heeray Mooti. All rights reserved 2024.</p>
       <p>Email: mootiheeray@gmail.com</p>
       <div className="icons">
-        <a href="https://www.instagram.com/heeray_mooti/" target="_blank" rel="noopener noreferrer" className="iconLink">
+        <a
+          href="https://www.instagram.com/heeray_mooti/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="iconLink"
+        >
           <FaInstagram />
         </a>
-        <a href="https://wa.me/+923377773131" target="_blank" rel="noopener noreferrer" className="iconLink">
+        <a
+          href="https://wa.me/+923359420926"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="iconLink"
+        >
           <FaWhatsapp />
         </a>
         <a href="mailto:mootiheeray@gmail.com" className="iconLink">
@@ -20,6 +30,6 @@ function Footer() {
       </div>
     </footer>
   );
-};
+}
 
 export default Footer;
