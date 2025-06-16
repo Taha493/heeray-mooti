8df4736ecd51f0e3004e93d05a6ed934bcf5fc9d
Taha Mahmood <69084443+Taha493@users.noreply.github.com>
2025-06-17 00:05:31 +0500

    Update Sidebar.js

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { FaInstagram, FaWhatsapp, FaEnvelope } from 'react-icons/fa'; // Using react-icons for icons
-import './index.css'
+import React from "react";
+import { FaInstagram, FaWhatsapp, FaEnvelope } from "react-icons/fa"; // Using react-icons for icons
+import "./index.css";
 function Sidebar({ isOpen, toggleSidebar, onCategorySelect }) {
   const handleCategoryClick = (category) => {
     onCategorySelect(category);
@@ -8,35 +8,60 @@ function Sidebar({ isOpen, toggleSidebar, onCategorySelect }) {
   };
 
   return (
-    <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+    <div className={`sidebar ${isOpen ? "open" : ""}`}>
       <button className="toggle-button" onClick={toggleSidebar}>
         ☰
       </button>
       <ul>
         <li onClick={() => handleCategoryClick(null)}>Home</li>
-        <li onClick={() => handleCategoryClick('Handmade Ring')}>Handmade Rings</li>
-        <li onClick={() => handleCategoryClick('Handmade Earring')}>Handmade Earrings</li>
-        <li onClick={() => handleCategoryClick('Handmade Set')}>Handmade Sets</li>
-        <li onClick={() => handleCategoryClick('Premade Ring')}>Premade Rings</li>
-        <li onClick={() => handleCategoryClick('Premade Earring')}>Premade Earrings</li>
-        <li onClick={() => handleCategoryClick('Premade Chain')}>Premade Chains</li>
-        <li onClick={() => handleCategoryClick("Resin Jewellery")}>Resin Jewellery</li>
-
-
+        <li onClick={() => handleCategoryClick("Handmade Ring")}>
+          Handmade Rings
+        </li>
+        <li onClick={() => handleCategoryClick("Handmade Earring")}>
+          Handmade Earrings
+        </li>
+        <li onClick={() => handleCategoryClick("Handmade Set")}>
+          Handmade Sets
+        </li>
+        <li onClick={() => handleCategoryClick("Premade Ring")}>
+          Premade Rings
+        </li>
+        <li onClick={() => handleCategoryClick("Premade Earring")}>
+          Premade Earrings
+        </li>
+        <li onClick={() => handleCategoryClick("Premade Chain")}>
+          Premade Chains
+        </li>
+        <li onClick={() => handleCategoryClick("Resin Jewellery")}>
+          Resin Jewellery
+        </li>
+        <li onClick={() => handleCategoryClick("Resin Jewellery")}>
+          Gift Boxes
+        </li>
       </ul>
       <div className="social-icons">
-      <a href="https://www.instagram.com/heeray_mooti/" target="_blank" rel="noopener noreferrer" className="iconLink">
+        <a
+          href="https://www.instagram.com/handycollection2024/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="iconLink"
+        >
           <FaInstagram />
         </a>
-        <a href="https://wa.me/+923377773131" target="_blank" rel="noopener noreferrer" className="iconLink">
+        <a
+          href="https://wa.me/+923025975556"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="iconLink"
+        >
           <FaWhatsapp />
         </a>
-        <a href="mailto:mootiheeray@gmail.com" className="iconLink">
+        <a href="mailto:hycollection6@gmail.com" className="iconLink">
           <FaEnvelope />
         </a>
       </div>
     </div>
   );
-};
+}
 
 export default Sidebar;
